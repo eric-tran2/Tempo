d72dc7ae552b51cb2ebc7c557dc1ead74a45d514
Eric Tran <eric.tran119@gmail.com>
2021-10-08 14:20:10 -0400

    added check mark and trash can button

--- src/index.js
+++ src/index.js
@@ -26,13 +26,13 @@ document.addEventListener('DOMContentLoaded', () => {
     todoDiv.appendChild(newTodo);
 
     const completedButton = document.createElement('button');
-    completedButton.innerHTML = '<i class=fas fa-check"></i>';
+    completedButton.innerHTML = '<i class="fa fa-check"></i>';
     completedButton.classList.add("complete-btn");
     todoDiv.appendChild(completedButton);
 
     
     const trashButton = document.createElement('button');
-    trashButton.innerHTML = '<i class=fas fa-trash"></i>';
+    trashButton.innerHTML = '<i class="fa fa-trash"></i>';
     trashButton.classList.add("complete-btn");
     todoDiv.appendChild(trashButton);
 

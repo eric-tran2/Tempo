ca6bcbd0c72d778fcb6dbeaae4d4e940e88052c0
Eric Tran <eric.tran119@gmail.com>
2021-10-12 21:15:21 -0400

    testing why filters dont work

--- src/index.js
+++ src/index.js
@@ -62,27 +62,28 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  function filterTodo(e){
+  function filterTodo(e) {
     const todos = todoList.childNodes;
-    todos.forEach(function(todo){
-      switch(e.target.value){
-        case "all": 
-        todo.style.display = "flex";
+    todos.forEach(function (todo) {
+      switch (e.target.value) {
+        case "all":
+          todo.style.display = "flex";
           break;
-        case "finished":
-          if (todo.classList.contains('finished')){
-            todo.style.display = 'flex';
+        case "completed":
+          if (todo.classList.contains("completed")) {
+            todo.style.display = "flex";
           } else {
             todo.style.display = "none";
           }
-        case "unfinished":
-          if (!todo.classList.contains('finished')){
-            todo.style.display = 'flex';
-          } else { 
+          break;
+        case "uncompleted":
+          if (!todo.classList.contains("completed")) {
+            todo.style.display = "flex";
+          } else {
             todo.style.display = "none";
           }
       }
-    })
+    });
   }
 })
 

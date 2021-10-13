c63c26b55f37cbc75c62a1f9b8b3c8ef7b1e35de
Eric Tran <eric.tran119@gmail.com>
2021-10-13 16:13:08 -0400

    duplicating function

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function addTodo(event){
     event.preventDefault();
+
     const todoDiv = document.createElement('div');
     todoDiv.classList.add("todo");
     
@@ -106,8 +107,32 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
       todos = JSON.parse(localStorage.getItem('todos'));
     }
-    todos.push(todo);
-    localStorage.setItem('todos', JSON.stringify(todos));
+    todos.forEach(function(todo){
+      const todoDiv = document.createElement('div');
+      todoDiv.classList.add("todo");
+
+      const newTodo = document.createElement('li');
+      newTodo.innerText = todo;
+
+      newTodo.classList.add('todo-item');
+      todoDiv.appendChild(newTodo);
+
+      saveToLocalStorage(todoInput.value);
+
+      const completedButton = document.createElement('button');
+      completedButton.innerHTML = '<i class="fa fa-check"></i>';
+      completedButton.classList.add("complete-btn");
+      todoDiv.appendChild(completedButton);
+
+
+      const trashButton = document.createElement('button');
+      trashButton.innerHTML = '<i class="fa fa-trash"></i>';
+      trashButton.classList.add("trash-btn");
+      todoDiv.appendChild(trashButton);
+
+      todoList.appendChild(todoDiv);
+
+    });
   }
 })
 

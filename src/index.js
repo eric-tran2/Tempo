440c57372d5ed701f86770f543800adc50fa8d9b
Eric Tran <eric.tran119@gmail.com>
2021-10-12 22:58:31 -0400

    creating save to local storage

--- src/index.js
+++ src/index.js
@@ -85,6 +85,17 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   }
+
+  function saveToLocalStorage(todo) {
+    let todos;
+    if (localStorage.getItem('todos') === null) {
+      todos = [];
+    } else {
+      todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }
 })
 
 

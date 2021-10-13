85a899747681eb848c1a070796c46164f69c36e4
Eric Tran <eric.tran119@gmail.com>
2021-10-13 16:04:11 -0400

    reseting

--- src/index.js
+++ src/index.js
@@ -99,16 +99,7 @@ document.addEventListener('DOMContentLoaded', () => {
     localStorage.setItem('todos', JSON.stringify(todos));
   }
 
-  function getTodos() {
-    let todos;
-    if (localStorage.getItem('todos') === null) {
-      todos = [];
-    } else {
-      todos = JSON.parse(localStorage.getItem('todos'));
-    }
-    todos.push(todo);
-    localStorage.setItem('todos', JSON.stringify(todos));
-  }
+  
 })
 
 

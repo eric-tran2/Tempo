7699717d2b392e076458e0e26b443a7fe0fdb0e5
Eric Tran <eric.tran119@gmail.com>
2021-10-20 18:56:16 -0400

    working on timer css

--- src/index.js
+++ src/index.js
@@ -191,8 +191,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
   class Timer {
     constructor(root) {
-      // root.innerHTML = Timer.getHTML();
-      // root.innerHTML = document.querySelector(".timer");
       this.el = {
         minutes: root.querySelector(".timer__part--minutes"),
         seconds: root.querySelector(".timer__part--seconds"),
@@ -232,11 +230,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     updateInterfaceControls() {
       if (this.interval === null) {
-        this.el.control.innerHTML = `<span class="material-icons">play_arrow</span>`;
+        // this.el.control.innerHTML = `<span class="material-icons">play_arrow</span>`;
         this.el.control.classList.add("timer__btn--start");
         this.el.control.classList.remove("timer__btn--stop");
       } else {
-        this.el.control.innerHTML = `<span class="material-icons">pause</span>`;
+        // this.el.control.innerHTML = `<span class="material-icons">pause</span>`;
         this.el.control.classList.add("timer__btn--stop");
         this.el.control.classList.remove("timer__btn--start");
       }
@@ -264,20 +262,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
       this.updateInterfaceControls();
     }
-
-    // static getHTML() {
-    //   return `
-		// 	<span class="timer__part timer__part--minutes">00</span>
-		// 	<span class="timer__part">:</span>
-		// 	<span class="timer__part timer__part--seconds">00</span>
-		// 	<button type="button" class="timer__btn timer__btn--control timer__btn--start">
-		// 		<span class="material-icons">play_arrow</span>
-		// 	</button>
-		// 	<button type="button" class="timer__btn timer__btn--reset">
-		// 		<span class="material-icons">timer</span>
-		// 	</button>
-		// `;
-    // }
   }
 
   new Timer(
